35238aabec7284f21008a9fce334c13c4bfd690d
Sevendel <harlee.berree@gmail.com>
2024-09-22 04:26:58 +0100

    feat: Init Prisma schema file

--- app/user/[slug]/page.tsx
+++ app/user/[slug]/page.tsx
@@ -1,30 +1,31 @@
-// import { NextPage } from 'next';
+import { NextPage } from 'next';
 
-// interface UserProps {
-//   params: {
-//     slug: string;
-//   };
-// }
+interface UserProps {
+  params: {
+    slug: string;
+  };
+}
 
-// const User: NextPage<UserProps> = ({ params }) => {
-//   return (
-//     <div>
-//       My slug is: {params.slug}
-//     </div>
-//   );
-// }; below is comprehensive tutorial
-// https://www.digitalocean.com/community/tutorials/create-dynamic-routes-in-next-js-with-route-guards-and-user-authentication
-
-// export default User;
-
-import Dashboard from './dashboard';
-
-export default function User() {
-  // { params }: { params: { slug: string } }
+const User: NextPage<UserProps> = ({ params }) => {
   return (
     <div>
-      {/* <h1>Welcome, {params.slug}!</h1> */}
-      <Dashboard />
+      My slug is: {params.slug}
     </div>
   );
-}
+}; 
+// below is comprehensive tutorial
+// https://www.digitalocean.com/community/tutorials/create-dynamic-routes-in-next-js-with-route-guards-and-user-authentication
+
+export default User;
+
+// import Dashboard from './dashboard';
+
+// export default function User() {
+//   { params }: { params: { slug: string } }
+//   return (
+//     <div>
+//       <h1>Welcome, {params.slug}!</h1>
+//       <Dashboard />
+//     </div>
+//   );
+// }
--- lib/auth.ts
+++ lib/auth.ts
@@ -4,3 +4,37 @@ import Google from "next-auth/providers/google"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
 })
+
+// import NextAuth from "next-auth"
+// import Google from "next-auth/providers/google"
+// import Credentials from "next-auth/providers/credentials"
+
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [
+//     Google,
+//     Credentials({
+//       credentials: {
+//         email: {},
+//         password: {},
+//       },
+//       authorize: async (credentials) => {
+//         let user = null
+ 
+//         // logic to salt and hash password
+//         // const pwHash = saltAndHashPassword(credentials.password)
+ 
+//         // logic to verify if the user exists
+//         // user = await getUserFromDb(credentials.email, pwHash)
+ 
+//         if (!user) {
+//           // No user found, so this is their first attempt to login
+//           // meaning this is also the place you could do registration
+//           throw new Error("User not found.")
+//         }
+ 
+//         // return user object with their profile data
+//         return user
+//       },
+//     }),
+//   ],
+// })
